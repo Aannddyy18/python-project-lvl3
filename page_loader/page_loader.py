8b33d87f5bdc93df71315d07a9562903f0b68a71
Андрей Трушин <trand1@mail.ru>
2022-04-10 03:37:49 +0700

    try to make 2 step

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,18 +1,56 @@
 import os
-import requests
+import sys
 import re
+import requests
+from urllib.parse import urlparse, urljoin
+from bs4 import BeautifulSoup
 
 
-def download(_url: str, dir_path=os.getcwd()):
-    cut_url = os.path.split(_url)
-    if cut_url[1].endswith(('.pdf', '.html', 'jpg', 'txt')):
+def save_change(soup, net_loc, pagefolder, url, tag, inner):
+    if not os.path.exists(pagefolder):  # create only once
+       try:
+            os.mkdir(pagefolder)
+       except Exception as exc:
+           print(exc, file=sys.stderr)
+    for res in soup.findAll(tag):  # images
+        if res.has_attr(inner):  # check inner tag
+            try:
+                fileurl = urljoin(url, res.get(inner))
+                filename, ext = os.path.splitext(res[inner])  # get name and extension
+                filename = re.sub('[^a-z0-9]', '-', net_loc + filename) + ext
+                filepath = os.path.join(pagefolder, filename)
+                # rename html ref
+                res[inner] = os.path.join(pagefolder, filename)
+                if not os.path.isfile(filepath):  # was not downloaded
+                    with open(filepath, 'wb') as file:
+                        session = requests.Session()
+                        filebin = session.get(fileurl)
+                        file.write(filebin.content)
+            except Exception as exc:
+                print(exc, file=sys.stderr)
+
+
+def download(url, dir_path=os.getcwd()):
+    o = urlparse(url)
+    net_loc = o.netloc
+    split_path = os.path.splitext(o.path)
+    cut_url = os.path.split(url)
+    if split_path[1] != '':
         cut_url = os.path.splitext(cut_url[1])
-        file_name = re.sub('[^a-z0-9]', '-', cut_url[0])
+        page_name = re.sub('[^a-z0-9]', '-', (net_loc + '/' + cut_url[0]))
+    elif split_path[1] == '' and split_path[0] == '':
+        page_name = re.sub('[^a-z0-9]', '-', net_loc)
     else:
-        file_name = re.sub('[^a-z0-9]', '-', cut_url[1])
-    file_name += '.html'
-    file_path = os.path.join(dir_path, file_name)
-    r = requests.get('https://google.com')
-    with open(file_path, 'wb') as f:
-        f.write(r.content)
+        page_name = re.sub('[^a-z0-9]', '-', (net_loc + '/' + cut_url[1]))
+    pagefolder = page_name + '_files'
+    page_name += '.html'
+    file_path = os.path.join(dir_path, page_name)
+    session = requests.Session()
+    responce = session.get(url)
+    soup = BeautifulSoup(responce.text, "html.parser")
+    tags_inner = {'img': 'src'}
+    for tag, inner in tags_inner.items(): # saves resource files and rename refs
+        save_change(soup, net_loc, pagefolder, url, tag, inner)
+    with open(page_name, 'wb') as file: # saves modified html doc
+        file.write(soup.prettify('utf-8'))
     return file_path
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,29 +1,29 @@
 import os
 from os.path import dirname
 import tempfile
+import requests
 import requests_mock
+from bs4 import BeautifulSoup
 from page_loader.scripts.pageloader import download
-
+from page_loader.page_loader import save_change
 
 FIXTURES_FOLDER = 'fixtures'
 
 
 def get_content(path_to_file):
+    with open(path_to_file, "r") as f:
+        return f.read()
+
+
+def get_content_b(path_to_file):
     with open(path_to_file, "rb") as f:
         return f.read()
 
 
 expected_result = get_content(os.path.join(dirname(__file__), FIXTURES_FOLDER, 'expected_page.html'))
-
-
-def test_page_loader_content():
-    with requests_mock.Mocker() as m:
-        m.get('https://google.com', content=expected_result)
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            download('https://google.com', tmpdirname)
-            with open(os.path.join(tmpdirname, "google-com.html"), 'rb') as d:
-                downloaded_content = d.read()
-                assert downloaded_content == expected_result
+source_page = get_content(os.path.join(dirname(__file__), FIXTURES_FOLDER, 'source_page.html'))
+expected_image = get_content_b(os.path.join(dirname(__file__), FIXTURES_FOLDER, 'hexlet.png'))
+changed_page = get_content(os.path.join(dirname(__file__), FIXTURES_FOLDER, 'changed_page.html'))
 
 
 def test_page_loader_path_to_file():
@@ -31,3 +31,31 @@ def test_page_loader_path_to_file():
         actual_path = download('https://google.com', tmpdirname)
         expected_path = os.path.join(tmpdirname, "google-com.html")
         assert actual_path == expected_path
+
+
+def test_page_loader_img():
+    with requests_mock.Mocker() as m:
+        m.get('https://ru.hexlet.io/courses/assets/professions/nodejs.png', content=expected_image)
+        with tempfile.TemporaryDirectory():
+            soup = BeautifulSoup(source_page, "html.parser")
+            url = 'https://ru.hexlet.io/courses'
+            net_loc = 'ru.hexlet.io'
+            save_change(
+                        soup, net_loc, 'ru-hexlet-io-courses_files', url, 'img', 'src')
+            with open(
+                        os.path.join('ru-hexlet-io-courses_files',
+                                    'ru-hexlet-io-assets-professions-nodejs.png'
+                                    ), 'rb') as d:
+                downloaded_content = d.read()
+                assert downloaded_content == expected_image
+
+
+def test_page_loader_change():
+    with requests_mock.Mocker() as m:
+        m.get('https://ru.hexlet.io/courses', text=source_page)
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            download('https://ru.hexlet.io/courses', tmpdirname)
+            with open(
+                    os.path.join('ru-hexlet-io-courses.html'), 'r') as d:
+                downloaded_content = d.read()
+                assert downloaded_content == changed_page
