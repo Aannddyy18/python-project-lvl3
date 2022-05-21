ad6d7d00a89763d14540d3f47afb4c50c1428120
Андрей Трушин <trand1@mail.ru>
2022-05-21 20:57:43 +0700

    4 review lvl3

--- page_loader/storage.py
+++ page_loader/storage.py
@@ -1,5 +1,10 @@
+import os
+
+
 def save_resource(file_path, file_name):
     try:
+        output_dir = os.path.dirname(file_path)
+        os.makedirs(output_dir, exist_ok=True)
         with open(file_path, 'wb') as file:
             file.write(file_name)
     except OSError:
